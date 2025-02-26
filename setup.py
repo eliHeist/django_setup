c8bbf29cfd0b6735ba13c0cbcdd12ab83ab2e197
Elijah Triumph <eliyang256@outlook.com>
2025-02-26 23:37:00 +0300

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="djangosetupheist",
-    version="0.1",
+    version="0.2",
     packages=find_packages(),
     install_requires=["django", "django-environ"],
     entry_points={
