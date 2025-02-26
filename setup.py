daccb9ab237883721d6758b07e38a24fe9c8df59
Elijah Triumph <eliyang256@outlook.com>
2025-02-26 22:16:47 +0300

    first commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="django_setup",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=["django", "django-environ"],
+    entry_points={
+        "console_scripts": [
+            "djs=django_setup.cli:main",
+        ],
+    },
+    author="DakDot",
+    description="A Django project setup automation tool",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/eliHeist/django_setup",  # Change this if you publish it
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.5",
+)
